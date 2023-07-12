5a14a522c2b31e282eeebc758040115fd80f6c83
sabrif31 <sabriflorin@gmail.com>
2023-07-13 00:28:46 +0200

    ad get-heroes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: '/get-heroes/',
-  base: '/',
+  base: '/get-heroes/',
   plugins: [react()],
 })
